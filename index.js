da05226d06cb47f08c7913e0279629190d308d02
samrshook <71894634+samrshook@users.noreply.github.com>
2022-04-25 11:57:51 -0500

    Update index.js
    Start applying clean coding concepts. Used better explanatory variables, removed unnecessary comments, and removed one instance of bad mental mapping.

--- index.js
+++ index.js
@@ -5,47 +5,34 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
 
-// Create object to hold each company we will pull listings from *will use this down the road
-//const employers = [
-//    {
-//        name: '',
-//        address: '',
-//        base: ''
-//    }
-//]
 
-// Array for holding the job postings
-
-const postings = []
+const jobPostings = []
 
 // Homepage greeting for local host 
-
 app.get('/', (req, res) => {
     res.json('Welcome to the Data Analyst Job Postings API')
 })
 
 
-// Page where jobs will be listed
-
+// Job postings landing page
 app.get('/jobs', (req, res) => {
-    // res.json(postings)
     axios.get('https://jobs.memorialhermann.org/search-jobs/analyst?orgIds=5358&kt=1')
         .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            // Getting every a-tag containing 'Analyst'
+            // Grabbing every a-tag containing 'Analyst'
             $('a:contains("Analyst")', html).each(function () {
-                const job_title = $(this).children('h2').text()     //Job titles are in an h2-tag nested in the a-tag
+                const jobTitle = $(this).children('h2').text()     //Job titles are in an h2-tag nested in the a-tag
                 const url = 'https://jobs.memorialhermann.org/' + $(this).attr('href')
 
-                postings.push({
-                    job_title: job_title,
-                    url: url,
+                jobPostings.push({
+                    Job Title: jobTitle,
+                    URL: url,
                     // source: company.name
                 }) 
             })
-            res.json(postings)
+            res.json(jobPostings)
         }).catch(err => console.log(err))
 })
 
